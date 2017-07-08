535e389a64b8091bea13940932abd01ec141be48
misinoe <misinoe.t@gmail.com>
2017-07-09 02:58:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+var width = 600;
+var height = 600;
+
 var canvas = document.getElementById('canvas');
 var application = new PIXI.Application({
   view: canvas,
+  width: width,
+  height: height,
   backgroundColor: 0x888888
 });
+
+var stage = application.stage;
+
+var plane = new PIXI.Graphics();
+stage.addChild(plane);
+plane.beginFill(0xffffff);
+plane.drawRect(0, 0, width, height);
